0342be10c2322c0d6ef779e8690e7025e2352c79
杨飞宇 <yangfeiyu@iorcas.com>
2015-04-14 10:25:23 +0800

    Initial Commit

---
+++ FYPopAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FYPopAnimation
+//
+//  Created by yangfeiyu on 15-4-14.
+//  Copyright (c) 2015年 NJJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FYPopAnimation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FYPopAnimation
+//
+//  Created by yangfeiyu on 15-4-14.
+//  Copyright (c) 2015年 NJJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
